ad694bebbd2147d255997ed420fbe440255b4100
returnnju <wangnjuzhi@gmail.com>
2016-10-07 18:40:48 +0800

    Update 2750-鸡兔同笼.cpp

--- 2750/2750-鸡兔同笼.cpp
+++ 2750/2750-鸡兔同笼.cpp
@@ -2,22 +2,18 @@
 using namespace std;
 
 int main() {
-	int n = 0;
-	cin >> n;
-	while (n--) {
-		int number = 0;
-		cin >> number;
-		if (number < 2 || number % 2 != 0) {
-			cout << 0 << " " << 0;
+	int number = 0;
+	cin >> number;
+	if (number < 2 || number % 2 != 0) {
+		cout << 0 << " " << 0;
+	}
+	else {
+		if (number % 4 == 0) {
+			cout << number / 4 << " " << number / 2;
 		}
 		else {
-			if (number % 4 == 0) {
-				cout << number / 4 << " " << number / 2;
-			}
-			else {
-				cout << number / 4 + 1 << " " << number / 2;
-			}
+			cout << number / 4 + 1 << " " << number / 2;
 		}
-		cout << endl;
 	}
+	cout << endl;
 }
